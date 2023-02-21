1344f5250dc987f9725bfda781b530229dc91100
ahmedrashed1619 <ar9353062@gmail.com>
2023-02-22 00:30:04 +0200

    added another language

--- src/Massenger.jsx
+++ src/Massenger.jsx
@@ -5,7 +5,7 @@ export default function Massenger() {
   return (
     <>
         <div className="wha" style={{ position: 'fixed', zIndex: '9999999', top: '88%', right: '30px'    }}>
-          <a href="https://m.me/">
+          <a href="https://m.me/" target='_blank' rel="noreferrer">
             <img src={messanger} alt='messanger' className='whatsapp'/>
           </a>
         </div>
